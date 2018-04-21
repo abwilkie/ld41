581a9d1cae90274f6db0f780f99a9aec14d519a6
Henry Morgan <hmorgan@patientslikeme.com>
2018-04-21 14:06:47 -0400

    sorry

--- deploy/assets/tilemap/bricks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="bricks" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="../brick.jpg" width="64" height="64"/>
-</tileset>
