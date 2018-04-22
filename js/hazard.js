407bd78240acb3b3ae034433bc604f128e6b0392
Henry Morgan <hmorgan@patientslikeme.com>
2018-04-22 13:42:16 -0400

    add fire and hazard assets

---
+++ js/hazard.js
@@ -0,0 +1,20 @@
+class Hazard {
+ constructor(scene) {
+    // this.sprite = scene.physics.add.sprite(200, 200, 'player');
+    this.scene = scene;
+  }
+
+  static preload(scene) {
+    // hazard tile?
+    // scene.load.image('player', 'assets/octosprite.png', 'assets/player.json');
+  }
+
+  static createAnims(){
+  }
+
+  create() {
+  }
+
+  update() {
+  }
+}
