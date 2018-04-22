ba545bd60e9d910980e6aaeb0aed93f4b7bd1783
Alexandria Wilkie <awilkie@patientslikeme.com>
2018-04-22 15:21:59 -0400

    update assets (new color players and fix flame size)

--- deploy/assets/flame.tsx
+++ deploy/assets/flame.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="flame" tilewidth="64" tileheight="64" tilecount="5" columns="5">
- <image source="fire.png" width="320" height="64"/>
+<tileset name="flame" tilewidth="70" tileheight="70" tilecount="5" columns="5">
+ <image source="fire.png" width="350" height="70"/>
 </tileset>
