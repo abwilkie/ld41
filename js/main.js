fdac4bbf2277008a0658e507ec80a6516cf341fc
Alexandria Wilkie <awilkie@patientslikeme.com>
2018-04-21 22:37:19 -0400

    put in new background art

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function preload() {
     // map made with Tiled in JSON format
     this.load.tilemapTiledJSON('map', 'assets/map.json');
     // tiles in spritesheet
-    this.load.spritesheet('tiles', 'assets/tiles.png', {frameWidth: 70, frameHeight: 70});
+    this.load.spritesheet('tiles', 'assets/replacementtiles.png', {frameWidth: 70, frameHeight: 70});
     // simple coin image
     this.load.image('coin', 'assets/coinGold.png');
     // player animations
