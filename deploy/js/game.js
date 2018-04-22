91fb4875eb492057c765267a4ca0c50bfa509735
Henry Morgan <hmorgan@patientslikeme.com>
2018-04-22 13:42:05 -0400

    add fire and hazard assets

--- deploy/js/game.js
+++ deploy/js/game.js
@@ -13,6 +13,27 @@ var planet = {
     }
 };
 
+class Hazard {
+ constructor(scene) {
+    // this.sprite = scene.physics.add.sprite(200, 200, 'player');
+    this.scene = scene;
+  }
+
+  static preload(scene) {
+    // hazard tile?
+    // scene.load.image('player', 'assets/octosprite.png', 'assets/player.json');
+  }
+
+  static createAnims(){
+  }
+
+  create() {
+  }
+
+  update() {
+  }
+}
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
