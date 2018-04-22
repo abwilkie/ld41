3693ca6f3926f5c1b84cdeddd5f2658768ceee57
Sam Backus <sbackus@patientslikeme.com>
2018-04-22 14:04:35 -0400

    add tomb or horrors maps

---
+++ deploy/assets/coin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="coin" tilewidth="70" tileheight="70" tilecount="1" columns="1">
+ <image source="coin.png" width="70" height="70"/>
+</tileset>
---
+++ deploy/assets/replacementTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="replacementTiles" tilewidth="70" tileheight="70" tilecount="16" columns="4">
+ <image source="replacementtiles.png" width="280" height="280"/>
+</tileset>
